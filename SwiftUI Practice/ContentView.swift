2b9db0899dd7e8a4f8b63f6a238c4809285f426b
Kamil Wrobel <wrobelk@live.com>
2020-03-09 21:34:45 -0400

    Initial commit

---
+++ SwiftUI Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Practice
+//
+//  Created by Kamil Wrobel on 3/9/20.
+//  Copyright © 2020 Kamil Wrobel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
