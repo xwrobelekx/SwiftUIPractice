fc881a6ba22a55b06677ca77c40377cd280c1174
Kamil Wrobel <wrobelk@live.com>
2020-03-09 22:36:56 -0400

    added forms, button, textField, and picker

--- SwiftUI Practice/ContentView.swift
+++ SwiftUI Practice/ContentView.swift
@@ -9,8 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount = 0
+    @State private var name = ""
+    let students = ["Kamil", "Daniel", "David"]
+    @State private var selectedStudent = "yyy"
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack{
+                Form {
+                    Section {
+                        Text("Hello, \(name)")
+                        Text("Hello, \(name)")
+                        Text("Hello, \(selectedStudent)")
+                        Text("Tap count: \(tapCount)")
+                    }
+                    Section {
+                        ForEach(0 ..< 4) {
+                            Text("Hello, Row # \($0)")
+                        }
+                    }
+                    
+                    Section {
+                        
+                        TextField("Please enter your name", text: $name)
+                        
+                        
+                        Button("Tap Count \(tapCount)") {
+                            self.tapCount += 1
+                        }
+                    }
+                    
+                    Section {
+                        
+                        Picker("SELECT YOUR STUDENT", selection: $selectedStudent) {
+                            ForEach(0 ..< students.count){
+                                Text(self.students[$0]).tag($0)
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            .navigationBarTitle(Text("SwiftUI"))
+        }
     }
 }
 
